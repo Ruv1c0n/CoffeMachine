7258db53ed6c85ad7c425846abf04b1ef96f0917
Даниил Матвеев <ruv1con@yandex.ru>
2023-04-13 21:27:58 +0300

    Обновлен список напитков и добавлены недостающие

---
+++ CoffeMachine/Beverage/HotChocolate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeMachine.Beverage
+{
+    class HotChocolate : BeverageBase
+    {
+        public HotChocolate()
+        {
+            DESCRIPTION = "Горячий шоколад";
+        }
+
+        public override double GetCost()
+        {
+            return 120;
+        }
+    }
+}
---
+++ CoffeMachine/Beverage/Macciato.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeMachine.Beverage
+{
+    class Маккиато : BeverageBase
+    {
+        public Маккиато()
+        {
+            DESCRIPTION = "Маккиато";
+        }
+
+        public override double GetCost()
+        {
+            return 100;
+        }
+    }
+}
