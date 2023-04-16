ab9107ca592baa5761a48be83cadefc625afeeea
Даниил Матвеев <ruv1con@yandex.ru>
2023-04-16 21:28:07 +0300

    Обработка кол-ва для напитков

--- CoffeMachine/CoffeMachine/SelectBeverage.cs
+++ CoffeMachine/CoffeMachine/SelectBeverage.cs
@@ -16,7 +16,58 @@ namespace CoffeMachine.CoffeMachine
             }
             int drink = Convert.ToInt32(Console.ReadLine());
 
-            capacity.Espresso();
+            switch (drink)
+            {
+                case 1:
+                    {
+                        capacity.BoiledWater();
+                        break;
+                    }
+                case 2:
+                    {
+                        capacity.Espresso();
+                        break;
+                    }
+                case 3:
+                    {
+                        capacity.Americano();
+                        break;
+                    }
+                case 4:
+                    {
+                        capacity.Cappuccino();
+                        break;
+                    }
+                case 5:
+                    {
+                        capacity.Latte();
+                        break;
+                    }
+                case 6:
+                    {
+                        capacity.HotChocolate();
+                        break;
+                    }
+                case 7:
+                    {
+                        capacity.Cocoa();
+                        break;
+                    }
+                case 8:
+                    {
+                        capacity.Macciato();
+                        break;
+                    }
+                case 9:
+                    {
+                        capacity.FlatWhite();
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
 
             Console.WriteLine();
             return drink;
